c143338b6e31f9699fe8ba8dc37a90ad92dda5d5
Jesulayomy <jesulayomi081@gmail.com>
2023-03-10 19:59:10 +0000

    to push

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+""" Unittests for console application """
+
+import sys
+import unittest
+
+
+class TestConsoleApp(unittest.TestCase):
+    """ unittest class """
+
+    def setUp(self):
+        """ setup for each test """
+
+        pass
