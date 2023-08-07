ec6f1941525589429b998b40e9bf03dff00ef4e5
jesulayomy <jesulayomi081@gmail.com>
2023-08-07 17:59:32 +0100

    asyncio operations

--- pyhton_practice/snc.py
+++ pyhton_practice/snc.py
@@ -3,21 +3,21 @@
 import asyncio
 
 
-async def sleeperFunction(times):
-    """ Sleepwer dunctions """
+# async def sleeperFunction(times):
+#     """ Sleepwer dunctions """
 
-    print("Calling the generator")
-    for i in times:
-        print("Awaiting")
-        await asyncio.sleep(1)
-        yield i
+#     print("Calling the generator")
+#     for i in times:
+#         print("Awaiting")
+#         await asyncio.sleep(1)
+#         yield i
 
-async def nightTime(times):
-    print("Calling  the coroutine")
-    async for item in sleeperFunction(times):
-        print("Async For-ing")
-        print(item)
+# async def nightTime(times):
+#     print("Calling  the coroutine")
+#     async for item in sleeperFunction(times):
+#         print("Async For-ing")
+#         print(item)
 
-times = ["one", "two", "three", "four", "five"]
-asyncio.run(nightTime(times))
-print("Hello World")
+# times = ["one", "two", "three", "four", "five"]
+# asyncio.run(nightTime(times))
+# print("Hello World")
---
+++ pyhton_practice/summer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""  Sample function to test async operations """
+import asyncio
+
+
